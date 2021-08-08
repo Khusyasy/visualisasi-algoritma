964afeb7f0e9728272bb855b52b20b69a6f452de
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-08-08 07:05:45 +0700

    create algo selector

--- src/pages/Sort.js
+++ src/pages/Sort.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
 import random_array from "../scripts/utils/random_array";
 import bubble_sort from "../scripts/sorting/bubble_sort";
+import selection_sort from "../scripts/sorting/selection_sort";
 
 import SortingController from "../scripts/sorting/SortingController";
 import BarVisualizer from "../components/BarVisualizer/BarVisualizer";
 
 function Sort() {
-  const [values, setValues] = useState(random_array(25, 1337));
+  const [values, setValues] = useState(random_array(10, 123));
   const [statuses, setStatuses] = useState([]);
-  let RC = new SortingController(500, setValues, setStatuses);
+  let RC = new SortingController(250, setValues, setStatuses);
+
+  const [algo, setAlgo] = useState("bubble");
 
   function sort() {
-    bubble_sort(values, RC);
+    if(algo === "bubble") {
+      bubble_sort(values, RC);
+    }else if(algo === "selection") {
+      selection_sort(values, RC);
+    }
   }
 
   return (
     <div>
       <BarVisualizer values={values} statuses={statuses} />
-      <button onClick={sort}>sort</button>
+      <div className="controls">
+        <button onClick={sort}>sort</button>
+        <select name="algo" id="algo" value={algo} onChange={(e)=>setAlgo(e.target.value)}>
+          <option value="bubble">Bubble Sort</option>
+          <option value="selection">Selection Sort</option>
+        </select>
+      </div>
     </div>
   );
 }
