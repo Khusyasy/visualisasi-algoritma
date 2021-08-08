4ee56510cce1c63f79a7092cb3cffefeecff35e6
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2021-08-08 10:57:02 +0700

    overhaul array structure

--- src/scripts/utils/random_array.js
+++ src/scripts/utils/random_array.js
@@ -1,10 +1,17 @@
 const seedrandom = require("seedrandom");
+const { customAlphabet } = require("nanoid");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const nanoid = customAlphabet(alphabet, 3);
 
 function generate(n, seed) {
   let array = [];
 
   for(let i = 1; i <= n; i++) {
-    array.push(i);
+    array.push({
+      id: nanoid(),
+      value: i,
+      status: "",
+    });
   }
 
   let random = seedrandom(seed);
